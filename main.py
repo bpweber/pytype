190157a1b45798836c1fc0e5acebd1d387359c3e
Bryce Weber <bpweber98@gmail.com>
2024-09-13 01:42:32 -0700

    clean up variable names and function args

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def import_words():
     with open('words.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-def generate_phrase(phrase_len, words, diff, do_caps=True, do_punct=True):
-    str = ' '.join([words[random.randint(0, diff)] for _ in range(phrase_len)])
-    if do_caps:
+def generate_phrase(words, plen, diff, caps, punc):
+    str = ' '.join([words[random.randint(0, diff)] for _ in range(plen)])
+    if caps:
         str = str.capitalize()
-    if do_punct:
+    if punc:
         str += '.'
     return str
 
@@ -23,26 +23,26 @@ def score_input(phrase, inpt):
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')
     words = import_words()
-    do_caps, do_punct = '-caps' in sys.argv, '-punc' in sys.argv
+    caps, punc = '-caps' in sys.argv, '-punc' in sys.argv
     diff = re.search('-diff=[0-9]+', ' '.join(sys.argv))
     diff = min(int(diff.group().strip('-diff=')) if diff else len(words) - 1, len(words) - 1)
-    phrase_len = re.search('-len=[0-9]+', ' '.join(sys.argv))
-    phrase_len = min(int(phrase_len.group().strip('-len=')) if phrase_len else 15, 250)
-    print(diff, phrase_len)
+    plen = re.search('-len=[0-9]+', ' '.join(sys.argv))
+    plen = min(int(plen.group().strip('-len=')) if plen else 15, 250)
+    print(diff, plen)
     wpm_avg = 0
     accuracy_avg = 0
     ctr = 0
     while True:
         try:
-            phrase = generate_phrase(phrase_len, words, diff, do_caps, do_punct)
+            phrase = generate_phrase(words, plen, diff, caps, punc)
             start_t = time.time()
             inpt = input('\n' + phrase + '\n').strip()
             elapsed_t = time.time() - start_t
             accuracy, correct = score_input(phrase, inpt)
             wpm = 60 * correct / elapsed_t
+            wpm_avg = (ctr * wpm_avg + wpm) / (ctr + 1)
+            accuracy_avg = (ctr * accuracy_avg + accuracy) / (ctr + 1)
             ctr += 1
-            wpm_avg = ((ctr - 1) * wpm_avg + wpm) / (ctr)
-            accuracy_avg = ((ctr - 1) * accuracy_avg + accuracy) / (ctr)
             print(f'{round(100 * accuracy)}% | {round(accuracy * wpm)}wpm | {round(wpm)}raw')
         except KeyboardInterrupt:
             print(f'{round(100 * accuracy_avg)}% | {round(accuracy_avg * wpm_avg)}wpm | {round(wpm_avg)}raw')
