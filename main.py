51e8d66864e9f02f8fdc25b55ad4d146434e004f
Bryce Weber <bpweber98@gmail.com>
2024-09-12 23:19:28 -0700

    refactor scoring funtion

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ import sys
 import os
 
 def import_words():
-    words = []
     with open('words.txt', 'r') as f:
-        words = [line.strip() for line in f.readlines()]
-    return words
+        return [line.strip() for line in f.readlines()]
 
 def generate_phrase(phrase_len, words, do_caps=True, do_punct=True):
-    str = ''
-    for i in range(phrase_len):
-        str += words[random.randint(0, len(words) - 1)] + ' '
-    str = str.strip()
+    str = ' '.join([words[random.randint(0, len(words) - 1)] for _ in range(phrase_len)])
     if do_caps:
         str = str.capitalize()
     if do_punct:
@@ -21,40 +16,30 @@ def generate_phrase(phrase_len, words, do_caps=True, do_punct=True):
     return str
 
 def score_input(phrase, inpt):
-    inptlen = len(inpt)
-    correct_w = 0
-    phrase = phrase.split()
-    inpt = inpt.split()
-    for i, word in enumerate(phrase):
-        try:
-            if word == inpt[i]:
-                correct_w += 1
-        except:
-            pass
-    return correct_w / len(phrase), inptlen / 5
-                
+    correct_ch = sum(list(map(lambda x, y: sum(x == y for x, y in zip(x, y)), phrase.split(), inpt.split())))
+    return correct_ch / len(phrase.replace(' ', '')), len(inpt) / 5
+
 if __name__ == '__main__':
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     argv = sys.argv
-    do_caps = '-capitals' in argv
-    do_punct = '-punctuation' in argv
+    do_caps = '-caps' in argv
+    do_punct = '-punc' in argv
     words = import_words()
     phrase_len = 15
     wpm_avg = 0
     accuracy_avg = 0
-    phrase_cnt = 0
+    ctr = 0
     while True:
         try:
             phrase = generate_phrase(phrase_len, words, do_caps, do_punct)
             start_t = time.time()
             inpt = input('\n' + phrase + '\n').strip()
             elapsed_t = time.time() - start_t
-            correct = score_input(phrase, inpt)
-            wpm = 60 * correct[1] / elapsed_t
-            accuracy = correct[0]
-            phrase_cnt += 1
-            wpm_avg = ((phrase_cnt - 1) * wpm_avg + wpm) / (phrase_cnt)
-            accuracy_avg = ((phrase_cnt - 1) * accuracy_avg + accuracy) / (phrase_cnt)
+            accuracy, correct = score_input(phrase, inpt)
+            wpm = 60 * correct / elapsed_t
+            ctr += 1
+            wpm_avg = ((ctr - 1) * wpm_avg + wpm) / (ctr)
+            accuracy_avg = ((ctr - 1) * accuracy_avg + accuracy) / (ctr)
             print(f'{round(100 * accuracy)}% | {round(accuracy * wpm)}wpm | {round(wpm)}raw')
         except KeyboardInterrupt:
             print(f'{round(100 * accuracy_avg)}% | {round(accuracy_avg * wpm_avg)}wpm | {round(wpm_avg)}raw')
