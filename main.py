fc053e678725addbeebd2e0dbcc84317b123d0db
Bryce Weber <bpweber98@gmail.com>
2024-09-13 00:44:33 -0700

    add diff and len launch options

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import random
 import time
 import sys
 import os
+import re
 
 def import_words():
     with open('words.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-def generate_phrase(phrase_len, words, do_caps=True, do_punct=True):
-    str = ' '.join([words[random.randint(0, len(words) - 1)] for _ in range(phrase_len)])
+def generate_phrase(phrase_len, words, diff, do_caps=True, do_punct=True):
+    str = ' '.join([words[random.randint(0, diff)] for _ in range(phrase_len)])
     if do_caps:
         str = str.capitalize()
     if do_punct:
@@ -21,17 +22,19 @@ def score_input(phrase, inpt):
 
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')
-    argv = sys.argv
-    do_caps = '-caps' in argv
-    do_punct = '-punc' in argv
     words = import_words()
-    phrase_len = 15
+    do_caps, do_punct = '-caps' in sys.argv, '-punc' in sys.argv
+    diff = re.search('-diff=[0-9]+', ' '.join(sys.argv))
+    diff = min(int(diff.group().strip('-diff=')) if diff else len(words) - 1, len(words) - 1)
+    phrase_len = re.search('-len=[0-9]+', ' '.join(sys.argv))
+    phrase_len = min(int(phrase_len.group().strip('-len=')) if phrase_len else 15, 250)
+    print(diff, phrase_len)
     wpm_avg = 0
     accuracy_avg = 0
     ctr = 0
     while True:
         try:
-            phrase = generate_phrase(phrase_len, words, do_caps, do_punct)
+            phrase = generate_phrase(phrase_len, words, diff, do_caps, do_punct)
             start_t = time.time()
             inpt = input('\n' + phrase + '\n').strip()
             elapsed_t = time.time() - start_t
