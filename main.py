2bab8a951e18efee2d8b3461ebf81546ebbcb019
Bryce Weber <bpweber98@gmail.com>
2024-09-13 22:35:58 -0700

    add history to clear mode and make clear default mode

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ def import_words():
 
 def generate_phrase(words, plen, diff, caps, punc):
     str = ' '.join([words[random.randint(0, diff)] for _ in range(plen)])
-    if caps:
-        str = str[0].upper() + str[1:]
-    if punc:
-        str += '.'
+    if caps: str = str[0].upper() + str[1:]
+    if punc: str += '.'
     return str
 
 def score_input(phrase, inpt):
     correct_ch = sum(list(map(lambda x, y: sum(x == y for x, y in zip(x, y)), phrase.split(), inpt.split())))
     return correct_ch / len(phrase.replace(' ', '')), len(inpt) / 5
 
+def result_string(accuracy, wpm):
+    return f'{round(100 * accuracy)}% | {round(accuracy * wpm)}wpm | {round(wpm)}raw'
+
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')
     words = import_words()
-    caps, punc, clear = '-caps' in sys.argv, '-punc' in sys.argv, '-clear' in sys.argv
+    caps, punc, clear = '-caps' in sys.argv, '-punc' in sys.argv, '-noclear' not in sys.argv
     diff = re.search('-diff=[0-9]+', ' '.join(sys.argv))
     diff = min(int(diff.group().strip('-diff=')) if diff else 10, 10)
     plen = re.search('-len=[0-9]+', ' '.join(sys.argv))
@@ -32,21 +33,27 @@ if __name__ == '__main__':
     wpm_avg = 0
     accuracy_avg = 0
     ctr = 0
+    hist = []
     while True:
         try:
-            if clear:
-                os.system('cls' if os.name == 'nt' else 'clear')
+            if clear: os.system('cls' if os.name == 'nt' else 'clear')
             phrase = generate_phrase(words, plen, diff, caps, punc)
             start_t = time.time()
-            inpt = input('\n' + phrase + '\n').strip()
+            inpt = input(f'{phrase} ⏎\n').strip()
             elapsed_t = time.time() - start_t
             accuracy, correct = score_input(phrase, inpt)
             wpm = 60 * correct / elapsed_t
             wpm_avg = (ctr * wpm_avg + wpm) / (ctr + 1)
             accuracy_avg = (ctr * accuracy_avg + accuracy) / (ctr + 1)
+            s = result_string(accuracy, wpm) + '\n' 
+            hist.extend([f'{phrase} ⏎', inpt, s])
+            print(s)
             ctr += 1
-            print(f'{round(100 * accuracy)}% | {round(accuracy * wpm)}wpm | {round(wpm)}raw')
         except KeyboardInterrupt:
-            print(f'{round(100 * accuracy_avg)}% | {round(accuracy_avg * wpm_avg)}wpm | {round(wpm_avg)}raw')
+            sys.stdout.write('\033[F\033[K')
+            if clear: print('\n'.join(hist))
+            s = result_string(accuracy_avg, wpm_avg)
+            print('[Session Average]'.center(len(s)))
+            print(s)
             exit()
 
